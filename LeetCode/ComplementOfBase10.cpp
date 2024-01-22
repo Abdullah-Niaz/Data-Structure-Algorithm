26b7b9745ca3d4b1e250ebf7d8f9dafe9f8de9ad
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-22 19:03:24 +0500

    Complement of Base 10

---
+++ LeetCode/ComplementOfBase10.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int reverseInteger()
+{
+    int n = 65332;
+    int result = 0;
+
+    while (n != 0)
+    {
+        int digit = n % 10;
+        // -27 -.... + 27
+        if ((result > INT64_MAX / 10) || (result < INT64_MIN / 10))
+        {
+            return 0;
+        }
+        else
+        {
+            result = (result * 10) + digit;
+            n = n / 10;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    int r = reverseInteger();
+    cout << r;
+    return 0;
+}
