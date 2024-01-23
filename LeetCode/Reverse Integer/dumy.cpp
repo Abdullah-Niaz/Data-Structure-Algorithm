17164b88bb84655f145cda29ad909d645704a4d0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-23 11:55:48 +0500

    Reverse Integer

--- LeetCode/Reverse Integer/dumy.cpp
+++ LeetCode/Reverse Integer/dumy.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int reverseInteger(){
-    int n = 65332;
-    int result = 0;
-
-    while (n != 0)
-    {
-        int digit = n % 10 ;
-        // -27 -.... + 27
-        if((result > INT64_MAX/10) || ( result < INT64_MIN/10)){
-            return 0;
-        }
-        else{
-        result = ( result * 10 ) + digit;
-        n = n / 10;
-        }
-    }
-    return result;
-}
-
 int main(){
-    int r  = reverseInteger();
-    cout<<r;
+    int a = 5, b = 10, c = 15, d = 20;
+    if( a  > b || a > c || a > d){
+        cout<<"A is Greater : "<<a <<endl;
+    }
+    else if(b > c || b > d){
+        cout<<"B is Greater: "<<b<<endl;
+    }
+    else if(c > d ){
+        cout<<"C is Greater: "<<c<<endl;
+    }
+    else{
+        cout<<"D is Greater: "<<d;
+    }
     return 0;
 }
