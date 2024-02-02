70142fa5aa0f1b547e136f0b0e91e6296050ef3b
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-02 23:09:40 +0500

    Sum of linked List

---
+++ 07. Linked_List/dumy.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+void Display(struct Node *ptr){
+    while (ptr != NULL)
+    {
+        cout<<"Address is: "<<ptr->next<<endl;
+        cout<<"Value is : "<<ptr->data<<endl;
+        ptr = ptr->next;
+    }
+};
+
+int main(){
+    struct Node *head,*second,*third,*fourth;    
+    head = (struct Node *)(malloc(sizeof(struct Node )));
+    second = (struct Node *)(malloc(sizeof(struct Node )));
+    third = (struct Node *)(malloc(sizeof(struct Node )));
+    fourth = (struct Node *)(malloc(sizeof(struct Node )));
+
+    head->data = 20;
+    head->next = second;
+
+    second->data = 40;
+    second->next = third;
+
+    third->data = 60;
+    third->next = fourth;
+
+    fourth->data = 80;
+    fourth->next = NULL;
+
+    Display(head);
+    return 0;
+}
---
+++ 07. Linked_List/sumOfLinkedList.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void Display(struct Node *ptr)
+{
+    int count = 0;
+    while (ptr != NULL)
+    {
+        cout << "Address is: " << ptr->next << endl;
+        cout << "Value is : " << ptr->data << endl;
+        ptr = ptr->next;
+        count++;
+    }
+    cout << "Tottal Length of Node: " << count<<endl;
+};
+
+int add(struct Node *su){
+    int s =  0;
+    while (su != NULL)
+    {
+        s = s + su->data;
+        su = su->next;
+    }
+    return s;
+};
+int main()
+{
+    struct Node *head, *second, *third, *fourth;
+    head = (struct Node *)(malloc(sizeof(struct Node)));
+    second = (struct Node *)(malloc(sizeof(struct Node)));
+    third = (struct Node *)(malloc(sizeof(struct Node)));
+    fourth = (struct Node *)(malloc(sizeof(struct Node)));
+
+    head->data = 20;
+    head->next = second;
+
+    second->data = 40;
+    second->next = third;
+
+    third->data = 60;
+    third->next = fourth;
+
+    fourth->data = 80;
+    fourth->next = NULL;
+
+    Display(head);
+    int sumofLinkedList = add(head);
+    cout<<"Sum of the Linked List: "<<sumofLinkedList;
+    return 0;
+}
