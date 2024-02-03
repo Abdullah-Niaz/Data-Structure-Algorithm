1b3d25c72de552a368e1461f81cc1e136fa38ede
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-03 15:38:55 +0500

    linear Search

---
+++ 07. Linked_List/linearSearch.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+Node * linearSearch(struct Node *ptr, int key)
+{
+    while (ptr != NULL)
+    {
+        if (key == ptr->data)
+        {
+            key = ptr->data;
+            cout<<"key"<<ptr->data;
+            return ptr;
+        }
+        ptr = ptr->next;
+    }
+    return NULL;
+}
+
+int main()
+{
+    struct Node *head, *second, *third, *fourth;
+    head = (struct Node *)(malloc(sizeof(struct Node)));
+    second = (struct Node *)(malloc(sizeof(struct Node)));
+    third = (struct Node *)(malloc(sizeof(struct Node)));
+    fourth = (struct Node *)(malloc(sizeof(struct Node)));
+
+    head->data = 20;
+    head->next = second;
+
+    second->data = 40;
+    second->next = third;
+
+    third->data = 60;
+    third->next = fourth;
+
+    fourth->data = 80;
+    fourth->next = NULL;
+
+    struct Node *temp = linearSearch(head,40);
+    cout<<"Key found at: "<<temp;
+
+    return 0;
+}
