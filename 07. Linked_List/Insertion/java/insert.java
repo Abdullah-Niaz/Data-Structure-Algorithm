ac0355450dddfbb9f1c5edad4de4666d3f5bfcf6
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-03 23:43:11 +0500

    insertion in java

---
+++ 07. Linked_List/Insertion/java/insert.java
@@ -0,0 +1,61 @@
+package java;
+class Node{
+    int data;
+    Node next;
+
+    void TraverseLinkedList(Node ptr){
+        while (ptr != null)
+        {
+            System.out.println("Value is: " + ptr.data);
+            ptr = ptr.next;
+        }
+    };
+
+    Node insertAtFirst(Node head,int data){
+    Node ptr = new Node();
+    ptr.next = head;
+    ptr.data = data;
+    return ptr;
+};
+};
+
+
+public class insert{
+    public static void main(String[] args) {
+    Node head,second;
+    head = new Node();
+    second = new Node();
+
+
+    head.data = 10;
+    head.next = second;
+    second.data = 12;
+    second.next = null;
+
+
+    head.TraverseLinkedList(head);
+    head = head.insertAtFirst(head, 8);
+    head.TraverseLinkedList(head);
+
+    // TraverseLinkedList(head);
+    // head = insertAtFirst(head, 6);
+    // TraverseLinkedList(head);
+
+    // TraverseLinkedList(head);
+    // head = insertAtFirst(head, 4);
+    // TraverseLinkedList(head);
+
+    // TraverseLinkedList(head);
+    // head = insertAtFirst(head, 3);
+    // TraverseLinkedList(head);
+
+    // TraverseLinkedList(head);
+    // head = insertAtFirst(head, 2);
+    // TraverseLinkedList(head);
+
+    // TraverseLinkedList(head);
+    // head = insertAtFirst(head, 1);
+    // TraverseLinkedList(head);
+
+    }
+}
