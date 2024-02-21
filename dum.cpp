a42a5209c07714f9be71cbdd4e4634c1771c61be
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-21 23:37:18 +0500

    infix to postfic conversion

---
+++ dum.cpp
@@ -0,0 +1,28 @@
+// Function to convert infix expression to postfix
+string infixToPostfix(const string& infix) {
+    Stack stack;
+    create(&stack, infix.length()); 
+    string postfix = ""; 
+    for (char c : infix) {
+        if (isalnum(c)) {  
+            postfix += c;
+              // abc*+de
+        } else if (c == '(') {
+            push(&stack, c);
+        } else if (c == ')') { 
+            while (!isEmpty(&stack) && peek(&stack) != '(') {
+                postfix += pop(&stack);
+            }
+            pop(&stack);
+        } else { 
+            while (!isEmpty(&stack) && precedence(c) <= precedence(peek(&stack))) {
+                postfix += pop(&stack); 
+            }
+            push(&stack, c);
+        }
+    }
+    while (!isEmpty(&stack)) {
+        postfix += pop(&stack);
+    }
+    return postfix; 
+}
---
+++ dumy.java
@@ -0,0 +1,20 @@
+/**
+ * dumy
+ */
+interface car {
+
+    public void carengine();
+}
+
+class totyta implements car{
+    public void carengine(){
+        System.out.println("Car Engine Started");
+    }
+}
+
+
+public class dumy{
+    public static void main(String[] args) {
+        
+    }
+}
