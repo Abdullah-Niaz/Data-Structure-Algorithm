efecf4ba0ff98bf30a0d8fc6e437a070f88a7d88
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-25 23:48:48 +0500

    Greatest of three

--- LeetCode/Reverse Integer/dumy.cpp
+++ LeetCode/Reverse Integer/dumy.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     int a,b,c,d;
     cout<<"Enter a,b,c,d: "
+    cin>>a>>b>>c>>d;
     if( a  > b || a > c || a > d){
         cout<<"A is Greater : "<<a <<endl;
     }
