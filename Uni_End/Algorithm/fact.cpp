13529e26eea58b4d4d8051e798649d94055591e2
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-27 06:10:19 +0500

    Factorial

---
+++ Uni_End/Algorithm/fact.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 3;
+    int fact = 1;
+    for (int i = 1; i <= n ; i++)
+    {
+        fact *= i;
+    }
+    cout<<"Fact: "<<fact;
+    
+    return 0;
+}
