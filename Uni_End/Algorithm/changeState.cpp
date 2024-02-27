b344a2f901d5003fb4198f3d0a2f0a07600feb20
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-27 05:24:16 +0500

    Change State

---
+++ Uni_End/Algorithm/changeState.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float x  = -2.3;
+    if( x > 0){
+        x = int(x);
+    }
+    else{
+        x = int(x);
+    }
+    cout<<"x: "<<x;
+    return 0;
+}
