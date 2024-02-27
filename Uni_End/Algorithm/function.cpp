5790bb7b97212468a1f0f49dfc340f6c4867186a
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-02-27 06:10:44 +0500

    math func

---
+++ Uni_End/Algorithm/function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    
+    int x = 2.2;
+    cout<<"Ceil: "<<ceil(x)<<endl;
+    cout<<"Floor: "<<floor(x);
+    return 0;
+}
