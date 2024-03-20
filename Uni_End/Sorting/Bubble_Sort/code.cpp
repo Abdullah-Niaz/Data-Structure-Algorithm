9befb615e0c0510b6a74cc57f106fca22c549d13
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-20 22:33:45 +0500

    Insertion Sort

--- Uni_End/Sorting/Bubble_Sort/code.cpp
+++ Uni_End/Sorting/Bubble_Sort/code.cpp
@@ -8,7 +8,6 @@ void bubbleSort(int arr[], int n)
     for (int i = 0; i < n - 1; i++)
     {
         swapped = false;
-
         // Last i elements are already in place
         for (int j = 0; j < n - i - 1; j++)
         {
