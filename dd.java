b0c92cda85052d5e03fc0d1d5291109c9aa86615
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-21 11:55:36 +0500

    Sorting

--- dd.java
+++ dd.java
@@ -2,7 +2,7 @@ public class dd {
     public static void main(String[] args) {
         int arr[] = {1,2,4};
         int n = 3;
-        int[] p = arr;
+        // int[] p = arr;
         for(int i = 0 ; i < n ; i++){
             // p[i] = p[i] + p[i+1];
             System.out.println("value: "+ arr[i] + " with address: ");
