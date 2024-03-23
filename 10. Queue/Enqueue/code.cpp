b49e7a8123a8add62ea5d13b9f3e0fc89eb59c29
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-23 16:30:00 +0500

    enqueue

--- 10. Queue/Enqueue/code.cpp
+++ 10. Queue/Enqueue/code.cpp
@@ -20,6 +20,14 @@ void enqueue(Queue *q, int x) {
 };
 
 
+void display(struct Queue *q){
+    for (int i = q->front + 1; i <q->rare ; i++)
+    {
+        cout<<"Data Element at Index: "<<i<<" is: "<<q->Q[i] <<endl;
+    }
+    
+}
+
 int main(){
     struct Queue *q;
     q = (struct Queue * )(malloc(sizeof(struct Queue)));
@@ -27,11 +35,14 @@ int main(){
     cin>>q->size;
     // q->Q = (int *)malloc(sizeof(q->size * sizeof(int)));
     q->Q = new int[q->size];
-    cout<<"Memory Allocated Successfuly on run time of size: "<<q->size;
     q->front = -1;
     q->rare = -1;
 
 
-    enqueue( q , 5);
+    enqueue(q,5);
+    enqueue(q,15);
+    enqueue(q,25);
+    enqueue(q,35);
+    display(q);
     return 0;
 }
