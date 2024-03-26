27e3de1ab9700bdfb4d7d5277ff114c45579e190
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-26 22:40:38 +0500

    Testing 1M Numbers

---
+++ 16. Sorting_Techniques/01. Bubble_Sort/pp/testing.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <time.h>
+using namespace std;
+using namespace std::chrono;
+
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+};
+
+void BubbleSort(int A[], int size){
+    int flag; // to check whether swaping is done or not
+    auto start = high_resolution_clock::now();
+    for (int i = 0; i < size - 1; i++)
+    { 
+        flag = 0;
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if(A[j] > A[j + 1]){
+                swap(&A[j], &A[j+1]);
+                flag = 1;
+            }
+        }   
+        if (flag == 0)
+        {
+            break;
+        }
+        
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+   cout << "Time taken by Bubble Sort Algo: "
+         << duration.count() << " microseconds" << endl;
+}
+
+int main(){
+    const long size = 10000;
+    int A[size];
+    int count = 0;
+    for (int i = 0; i < size; ++i) {
+        A[i] = rand() % 60000 + 1; // Generates a random number between 1 and 1000
+        count++;
+    }
+    
+    BubbleSort(A, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout<<A[i] <<" ";
+    }
+    cout<<"\nTotal Values: "<<count;
+    
+    
+    // int A[] = {3,4,2,10,7, 6, 15};
+    // int size  = sizeof(A) / sizeof(A[0]);
+    // BubbleSort(A,size);
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<<A[i] <<" ";
+    // }
+
+    
+    return 0;
+
+}
