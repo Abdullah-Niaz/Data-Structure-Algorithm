d7661f12386650d28a7a3c6cb53d568ac8451ca2
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-27 00:27:31 +0500

    Insertion Sort

---
+++ 16. Sorting_Techniques/02. Insertion_Sort/code.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+
+void Insertion(int A[], int size){
+    // int key = A[0]; 
+    for (int i = 0; i < size; i++)
+    {
+        int key = A[i]; 
+        int j = i - 1;
+        while ((j >= 0) && (A[j] > key))
+        {
+            A[j + 1] = A[j];    
+            j--; 
+        }
+        A[j + 1] = key;
+    }
+}
+
+int main(){
+    int A[] = {6,8,12,14,18,20,22,2};
+    int n = sizeof(A) / sizeof(A[0]);
+
+
+
+    cout<<"\nActual Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<A[i]<< " ";
+    }
+    Insertion(A, n);
+    cout<<"\nSorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout<<A[i] << " ";
+    }
+    
+    return 0;
+}
