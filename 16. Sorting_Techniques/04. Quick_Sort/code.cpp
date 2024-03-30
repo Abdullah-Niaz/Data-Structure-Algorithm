a70833f0cb3981c8bbf4d0c055c5912d7eab26d5
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-03-30 13:21:45 +0500

    Quick Sort

--- 16. Sorting_Techniques/04. Quick_Sort/code.cpp
+++ 16. Sorting_Techniques/04. Quick_Sort/code.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void Swap(int *x , int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int partition(int A[], int l, int h){
     // l = 0 , h = n - 1
     int pivot = A[l]; // selecting first element, to place it on it's actual positionl
@@ -14,20 +20,20 @@ int partition(int A[], int l, int h){
 
         do{
             j--;
-        }while(A[j] < pivot);
+        }while(A[j] > pivot);
 
         if (i < j ){
             Swap(&A[i], &A[j]);
         }
     }while(i < j);
-    Swap(&A[l])  , &A[j] ;// placing the  pivot at its correct position
+    Swap(&A[l],&A[j]);// placing the  pivot at its correct position
     return j;
 }
 
 void QuickSort( int A[] , int l , int h ){
     int j;
     if ( l < h ){
-        j = partition(A, l, j);
+        j = partition(A, l, h);
         QuickSort(A,l,j);
         QuickSort(A,j+1,h);
     }
@@ -35,18 +41,18 @@ void QuickSort( int A[] , int l , int h ){
 
 int main(){
     int A[] = {11,13,7,12,16,8,24,5,19,3};
-    int n  sizeof(A)/sizeof(A[0]);
+    int n   = sizeof(A)/sizeof(A[0]);
 
     cout<<"Unsorted Array: ";
     for (int i = 0; i < n; i++)
     {
-        cout<<A[i];
+        cout<<A[i] << " ";
     }
-    QuickSort(A, 0, n-1);
-    cout<<"Sorted Array: ";
+    QuickSort(A, 0, n);
+    cout<<"\nSorted Array: ";
     for (int i = 0; i < n; i++)
     {
-        cout<<A[i];
+        cout<<A[i] << " ";
     }
     
     return 0;
