7b32fe2848b847dd826103b9e96e1ab7e66470c0
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-02 10:52:57 +0500

    Insertion Between

---
+++ Uni_End/linkedList/Single/insertion/between.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void display(struct Node *head){
+    struct Node *ptr = head;
+    while (ptr!=NULL)
+    {
+        cout<<"Data: "<< ptr->data<<endl;
+        cout<<"Addres: "<<ptr<<endl;
+        ptr = ptr->next;
+    }
+    
+};
+
+struct Node *InsertBetween(struct Node *head, int data, int index){
+    struct Node *p = head;
+    struct Node *ptr = new Node;
+    int i = 0 ;
+    while (i != index - 1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next = p->next ;
+    p->next = ptr;
+    return head;
+
+}
+int main(){
+    struct Node *n1,*n2,*n3;
+    n1 = (struct Node *)(malloc(sizeof(struct Node )));
+    n2 = (struct Node *)(malloc(sizeof(struct Node )));
+    n3 = (struct Node *)(malloc(sizeof(struct Node )));
+
+    n1->data = 20;
+    n1->next = n2;
+    n2->data = 30;
+    n2->next = n3;
+    n3->data = 40;
+    n3->next = NULL;
+
+    display(n1);
+    n1 = InsertBetween(n1,25,1);
+    display(n1);
+    return 0;
+}
