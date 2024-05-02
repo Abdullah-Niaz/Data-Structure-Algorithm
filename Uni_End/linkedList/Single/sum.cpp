97fe088564cde1a024fa394f28fc8e4983addb29
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-02 10:11:14 +0500

    Sum Nodes

---
+++ Uni_End/linkedList/Single/sum.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void display(struct Node *head){
+    struct Node *ptr = head;
+    while (ptr!=NULL)
+    {
+        cout<<"Data: "<< ptr->data<<endl;
+        cout<<"Addres: "<<ptr<<endl;
+        ptr = ptr->next;
+    }
+    
+};
+
+int Sum(struct Node *head){
+    struct Node *ptr = head;
+    int sum = 0 ;
+    while (ptr != NULL)
+    {
+        sum += ptr->data;
+        ptr = ptr->next;
+    }
+    return sum;
+}
+
+
+int main(){
+    struct Node *n1,*n2,*n3;
+    n1 = (struct Node *)(malloc(sizeof(struct Node )));
+    n2 = (struct Node *)(malloc(sizeof(struct Node )));
+    n3 = (struct Node *)(malloc(sizeof(struct Node )));
+
+    n1->data = 20;
+    n1->next = n2;
+    n2->data = 30;
+    n2->next = n3;
+    n3->data = 40;
+    n3->next = NULL;
+
+    display(n1);
+    int su = Sum(n1);
+    cout<<"Sum: "<<su;
+    return 0;
+}
