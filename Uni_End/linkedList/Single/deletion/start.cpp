a6a11e366c00d55ac01b7f4c2f95d16170ed0d5d
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-05-02 11:24:31 +0500

    Deletion From Start

---
+++ Uni_End/linkedList/Single/deletion/start.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void display(struct Node *head){
+    struct Node *ptr = head;
+    while (ptr!=NULL)
+    {
+        cout<<"Data: "<< ptr->data<<endl;
+        cout<<"Addres: "<<ptr<<endl;
+        ptr = ptr->next;
+    }
+    
+};
+
+struct Node * DeletefromStat(struct Node *head){
+    struct Node *ptr = head;
+    head= head->next;
+    free(ptr);
+    return head;
+};
+
+
+
+
+int main(){
+    struct Node *n1,*n2,*n3;
+    n1 = (struct Node *)(malloc(sizeof(struct Node )));
+    n2 = (struct Node *)(malloc(sizeof(struct Node )));
+    n3 = (struct Node *)(malloc(sizeof(struct Node )));
+
+    n1->data = 20;
+    n1->next = n2;
+    n2->data = 30;
+    n2->next = n3;
+    n3->data = 40;
+    n3->next = NULL;
+
+    display(n1);
+    n1 = DeletefromStat(n1);
+    display(n1);
+    return 0;
+}
